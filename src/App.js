2d06434a810bfc6bcc884f0e0b382f5c4caf8bf3
kywsc <fireyw@gmail.com>
2019-12-09 23:15:49 +0900

    -UseReducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import UseState from './components/UseState';
 import UseEffect from './components/UseEffect';
+import UseReducer from './components/UseReducer';
+import UseReducer2 from './components/UseReducer2';
 
 function App() {
 
@@ -10,6 +12,10 @@ function App() {
             <UseState/>
             <h4>useEffect</h4>
             <UseEffect/>
+            <h4>UseReducer</h4>
+            <UseReducer/>
+            <h4>UseReducer2</h4>
+            <UseReducer2/>
         </div>
     </>)
 
---
+++ src/components/UseReducer.js
@@ -0,0 +1,27 @@
+import React, {useReducer} from 'react';
+
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {value: state.value + 1};
+        case 'DECREMENT':
+            return {value: state.value - 1}
+    }
+}
+
+function UseReducer() {
+    const [state, dispatch]= useReducer(reducer, {value:0});
+
+    return(
+        <div>
+            <p>
+                현재 카운터 값은 <b>{state.value}</b>입니다
+            </p>
+            <button onClick={()=>dispatch({type:'INCREMENT'})}> </button>
+            <button onClick={()=>dispatch({type:'DECREMENT'})}> </button>
+        </div>
+    )
+}
+
+export default UseReducer;
