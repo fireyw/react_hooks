1bf7d35d6ce2493e3f7bc1179e2a22d81e83ede4
kywsc <fireyw@gmail.com>
2020-03-14 10:15:29 +0900

    regist on click

--- src/components/regist/MemberRegist.js
+++ src/components/regist/MemberRegist.js
@@ -1,6 +1,49 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
 const MemberRegist = () => {
+  const [email, setEmail] = useState();
+  const [pwd, setPwd] = useState();
+  const [name, setName] = useState();
+  const [birth, setBirth] = useState();
+  const [memberList, setMemberList] = useState([]);
+  const regist = () => {
+    const tempMember = {
+      email: "fireyw@naver.com",
+      pwd: "1111",
+      name: "yw",
+      birth: "840109"
+    };
+
+    const newMemberList = memberList.concat(tempMember);
+
+    setMemberList(newMemberList);
+  };
+  const onEmailChange = useCallback(
+    e => {
+      setEmail(e.target.value);
+    },
+    [email]
+  );
+
+  const onPwdChange = useCallback(
+    e => {
+      setPwd(e.target.value);
+    },
+    [pwd]
+  );
+  const onNameChange = useCallback(
+    e => {
+      setName(e.target.value);
+    },
+    [name]
+  );
+  const onBirthChange = useCallback(
+    e => {
+      setBirth(e.target.value);
+    },
+    [birth]
+  );
+
   return (
     <>
       <section style={{ margin: "30px" }}>
@@ -10,18 +53,34 @@ const MemberRegist = () => {
             type="text"
             name="email"
             placeholder="이메일 주소 입력"
+            onChange={onEmailChange}
           ></input>
         </div>
         <div>
-          <input type="text" name="pwd" placeholder="비밀번호"></input>
+          <input
+            type="text"
+            name="pwd"
+            onChange={onPwdChange}
+            placeholder="비밀번호"
+          ></input>
         </div>
         <div>
-          <input type="text" name="name" placeholder="이름"></input>
+          <input
+            type="text"
+            name="name"
+            onChange={onNameChange}
+            placeholder="이름"
+          ></input>
         </div>
         <div>
-          <input type="text" name="pwd" placeholder="비밀번호"></input>
+          <input
+            type="text"
+            name="birth"
+            onChange={onBirthChange}
+            placeholder="생일"
+          ></input>
         </div>
-        <button>회원 가입</button>
+        <button onClick={regist}>회원 가입</button>
       </section>
     </>
   );
